ecc19227e017ac9cb6d376b70f6ec23c60688d30
daniel-3063 <dzhu686@gmail.com>
2025-09-03 21:29:00 +1000

    Initial commit

--- main.ts
+++ main.ts
@@ -1,116 +1,150 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {App, Notice, Plugin, PluginSettingTab, Setting} from 'obsidian';
+import { moment } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface MistakeEntry {
+	count:number; // number of mistakes for this given entry
+	startDate:string; // start for given period
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+
+interface MistakeTrackerData {
+	// Settings data
+	displayMode: "since-date" | "clean";
+	displayFromIndex: number;
+
+	// Mistake data
+	mistakeHistory: MistakeEntry[];
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const DEFAULT_DATA: MistakeTrackerData = {
+	displayMode: 'since-date',
+	displayFromIndex: 0,
+	mistakeHistory: [{
+		startDate: moment().format('YYYY-MM-DD'),
+		count: 0
+	}]
+}
+
+export default class MistakeTrackerPlugin extends Plugin {
+	data: MistakeTrackerData;
+	private statusBarItem: HTMLElement;
+
+	updateStatusBar() {
+		// Handle empty array case
+		if (this.data.mistakeHistory.length === 0) {
+			this.statusBarItem.setText('No flags set');
+			return;
+		}
+
+		const entry = this.data.mistakeHistory[this.data.displayFromIndex];
+		let display = '';
+		if (this.data.displayMode === 'since-date') {
+			display = `Mistakes: ${entry.count} (since ${entry.startDate})`;
+		} else if (this.data.displayMode === 'clean') {
+			display = `Mistakes: ${entry.count}`;
+		}
+		this.statusBarItem.setText(display);
+	}
+
+	increaseMistakes() {
+		// cumulatively increment mistake count for all periods
+		for (let i = 0; i < this.data.mistakeHistory.length; i++) {
+			this.data.mistakeHistory[i].count++;
+		}
+	}
+
+	setNewFlag(){
+		// create new flag if possible
+		if(this.data.mistakeHistory.length < 1){
+			this.data.mistakeHistory.push({
+				startDate: moment().format('YYYY-MM-DD'),
+				count: 0
+			});
+			this.saveData();
+		} else if(this.data.mistakeHistory[this.data.mistakeHistory.length-1].startDate == moment().format('YYYY-MM-DD')) {
+			new Notice ("No duplicate flags allowed!");
+		} else {
+			new Notice ("flag created successfully");
+			this.data.mistakeHistory.push({
+				startDate: moment().format('YYYY-MM-DD'),
+				count: 0
+			});
+			this.saveData();
+		}
+	}
+
+	deleteFlag(indexToDelete: number) {
+		// Can't delete if no flags exist
+		if (this.data.mistakeHistory.length === 0) {
+			return;
+		}
+
+		// Remove the flag at the specified index
+		this.data.mistakeHistory.splice(indexToDelete, 1);
+
+		// Fix displayFromIndex if needed
+		if (this.data.displayFromIndex === indexToDelete) {
+			// If we deleted the displayed flag, show index 0 (or first available)
+			this.data.displayFromIndex = 0;
+		} else if (this.data.displayFromIndex > indexToDelete) {
+			// If we deleted a flag before the displayed one, shift down
+			this.data.displayFromIndex--;
+		}
+
+		// If no flags left, reset to 0
+		if (this.data.mistakeHistory.length === 0) {
+			this.data.displayFromIndex = 0;
+		}
+
+		this.saveData();
+		this.updateStatusBar();
+	}
 
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		await this.loadData();
+		this.statusBarItem = this.addStatusBarItem();
+		this.updateStatusBar();
+		
+		this.addCommand({
+			id: 'add-mistake',
+			name: 'Add mistake',
+			callback: () => {
+				this.increaseMistakes();
+				this.updateStatusBar();
+				this.saveData();
+			}
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addCommand({
+			id:"set-flag",
+			name: "set new flag date",
+			callback: () => {
+				this.setNewFlag();
+				this.updateStatusBar();
+			}
+		})
 	}
 
-	onunload() {
-
+	async loadData() {
+		this.data = Object.assign({}, DEFAULT_DATA, await super.loadData());
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	async saveData() {
+		await super.saveData(this.data);
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+	async onunload() {
+		await this.saveData();
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: MistakeTrackerPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: MistakeTrackerPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -121,14 +155,91 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+			.setName('Display mode')
+			.setDesc('Display from a specific date i.e 9 mistakes since 1967-06-07 OR display without the date')
+			.addDropdown(dropdown => dropdown
+				.addOption('since-date', 'Show date (9 mistakes since 1967-06-07)')
+				.addOption('clean', 'Clean display (9 mistakes)')
+				.setValue(this.plugin.data.displayMode)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
+					this.plugin.data.displayMode = value as 'since-date' | 'clean';
+					await this.plugin.saveData();
+					this.plugin.updateStatusBar();
 				}));
+
+		new Setting(containerEl)
+			.setName('Display from index')
+			.setDesc('Which flag to display mistakes from (0 = total, 1 = first flag, etc.)')
+			.addDropdown(dropdown => {
+				// Handle empty history
+				if (this.plugin.data.mistakeHistory.length === 0) {
+					dropdown.addOption('0', 'No flags available');
+					dropdown.setDisabled(true);
+					return;
+				}
+
+				// Add options for each flag
+				this.plugin.data.mistakeHistory.forEach((entry, index) => {
+					let label;
+
+					if (index === 0) {
+						label = `${index}: Total (${entry.startDate})`;
+					} else {
+						label = `${index}: ${entry.startDate}`;
+					}
+
+					dropdown.addOption(index.toString(), label);
+				});
+
+				dropdown.setValue(this.plugin.data.displayFromIndex.toString())
+					.onChange(async (value) => {
+						this.plugin.data.displayFromIndex = parseInt(value);
+						await this.plugin.saveData();
+						this.plugin.updateStatusBar();
+					});
+			});
+
+
+		new Setting(containerEl)
+			.setName('Delete this flag')
+			.addDropdown(dropdown => {
+				// Handle empty history
+				if (this.plugin.data.mistakeHistory.length === 0) {
+					dropdown.addOption('0', 'No flags available');
+					dropdown.setDisabled(true);
+					return;
+				}
+
+				// Add options for each flag
+				this.plugin.data.mistakeHistory.forEach((entry, index) => {
+					let label;
+
+					if (index === 0) {
+						label = `${index}: Total (${entry.startDate})`;
+					} else {
+						label = `${index}: ${entry.startDate}`;
+					}
+
+					dropdown.addOption(index.toString(), label);
+				});
+
+				dropdown.setValue("0");
+			})
+			.addButton(button => {
+				button.onClick(()=>{
+					const dropdown = button.buttonEl.parentElement?.querySelector('select') as HTMLSelectElement;
+					const selectedIndex = parseInt(dropdown.value);
+
+					if(selectedIndex === 0){
+						new Notice ("You cannot delete the default flag!");
+					} else {
+						this.plugin.deleteFlag(selectedIndex);
+						this.display();
+						new Notice ("Flag deletion successful!")
+					}
+
+				})
+			})
+		;
 	}
 }
